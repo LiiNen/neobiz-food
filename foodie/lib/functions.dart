fee49c7c60c2401cf443faef35638c7633b9f4ab
LiiNen <kjeonghoon065@gmail.com>
2021-08-20 18:50:35 +0900

    Initial commit (foodie)

---
+++ foodie/lib/functions.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void showToast(String message){
+  Fluttertoast.showToast(msg: message,
+      gravity: ToastGravity.BOTTOM
+  );
+}
+
+TextStyle textStyle({color: Colors.black, weight: int, size: double}) {
+  FontWeight fontWeight = FontWeight.normal;
+  switch(weight) {
+    case 400:
+      fontWeight = FontWeight.w400;
+      break;
+    case 600:
+      fontWeight = FontWeight.w600;
+      break;
+    case 700:
+      fontWeight = FontWeight.w700;
+      break;
+  }
+  return TextStyle(
+      color: color,
+      fontWeight: fontWeight,
+      fontFamily: "AppleSDGothicNeo",
+      fontStyle:  FontStyle.normal,
+      fontSize: size
+  );
+}
+
+textFieldClear(TextEditingController controller) {
+  controller.clear();
+}
