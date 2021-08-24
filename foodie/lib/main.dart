f58dfd32bbe5651be752e55d8e9e4606a41ef6a5
LiiNen <kjeonghoon065@gmail.com>
2021-08-24 17:40:48 +0900

    Add api tests

--- foodie/lib/main.dart
+++ foodie/lib/main.dart
@@ -1,34 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:foodie/statelessWidgets.dart';
+import 'restApi.dart' as api;
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MainPage()
+        home: MainPage()
     );
   }
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPage();
+}
+
+class _MainPage extends State<MainPage> {
+
+  @override
+  void initState() {
+    api.trialScrap();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MainAppBar(),
---
+++ foodie/lib/restApi.dart
@@ -0,0 +1,88 @@
+// https://flutter.dev/docs/cookbook/networking/fetch-data
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+String baseUrl = 'https://www.epicure.co.kr';
+String route = '/android/';
+
+String path_login = "usr/login.php";
+String path_regist = "usr/regist.php";
+String path_forget = "usr/reqlogin.php";
+String path_buy_coupon = "usr/buy_coupon.php";
+String path_use_coupon = "usr/use_coupon.php";
+String path_gift_coupon = "usr/gift_coupon.php";
+String path_send_coupon = "usr/send_coupon.php";
+String path_region_list = "service/region.php";
+String path_detail = "service/detail.php";
+String path_town = "service/town.php";
+String path_subway = "service/subway.php";
+String path_scrap = "service/scrap.php";
+String path_trial_scrap = "service/trial_scrap.php";
+String path_check = "service/check.php";
+String path_surround = "service/surround.php";
+String path_evaluate = "service/evaluate.php";
+String path_report = "service/report.php";
+String path_qna = "service/qna.php";
+String path_coupon = "service/coupon.php";
+String path_fcm = "service/fcm.php";
+String path_region3rd = "service/region_3rd.php";
+String path_search = "service/search.php";
+String path_info_change = "usr/change_info.php";
+String path_my_evaluation = "usr/my_evaluation.php";
+String path_find_user = "usr/find_user.php";
+String path_change_evaluation = "service/change_evaluation.php";
+String path_check_premium_user = "service/check_premium_user.php";
+String path_check_my_coupon_list = "usr/my_coupon_list.php";
+String path_check_my_received_coupon_list = "usr/my_received_coupon_list.php";
+String path_review_list = "usr/review_list.php";
+String path_invisible_coupon = "usr/invisible_coupon.php";
+String path_find_sns_user = "usr/find_sns_user.php";
+String path_error_log = "service/error_logger.php";
+String path_check_version_code = "service/check_version_code.php";
+String path_check_passwd = "usr/check_passwd.php";
+String path_find_dong_list = "service/find_dong_list.php";
+
+
+// Future login() async {
+//   final _query = {
+//     'mode': 'LOGIN',
+//     'id': 'epicure',
+//     'pw': '584472'
+//   };
+//   final _body = {
+//     'id': 'epicure',
+//     'pw': '584472'
+//   };
+//   var uri = Uri.parse('$baseUrl$path_login');
+//
+//
+//   var response = await http.post(Uri.parse('$baseUrl$route$path_login?mode=login'),
+//     body: '{"id": "epicure", "passwd": "584472"}',
+//
+//   );
+//
+//   if(response.statusCode == 200) {
+//     print(response.body);
+//     return response.body;
+//   }
+//   else {
+//     print('error');
+//     return response.body;
+//   }
+// }
+
+Future trialScrap() async {
+  final response = await http.get(Uri.parse('$baseUrl$route$path_trial_scrap'));
+  if(response.statusCode == 200) {
+    print(json.decode(response.body));
+    print(json.decode(response.body)['content'][0]);
+    return json.encode(response.body);
+  }
+  else {
+    print('error');
+    return response.body;
+  }
+}
